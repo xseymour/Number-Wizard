df17dda056e912ce61298c31d78cf03f21ed7e52
Xavier Seymour <xseymour3@gmail.com>
2018-05-14 14:23:35 -0400

    22. Refractor

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -9,30 +9,40 @@ public class NumberWizard : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		max++;
-		guess = (max + min) / 2;
-		print("Welcome to Number Wizard");
-		print("Pick a number in your head, but don't tell me!");
-		
-		print ("The highest number you can pick is " + max);
-		print ("The lowest number you can pick is " + min);
-
-		print("Is the number higher or lower than "+guess+"?");
-		print ("Up = higher, Down = lower, return = equal");
+		StartGame();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.UpArrow)) {
 			min = guess;
-			guess = (max + min) / 2;
-			print("Higher or lower than " + guess + "?");
+			NextGuess();
 		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
 			max = guess;
-			guess = (max + min) / 2;
-			print("Higher or lower than " + guess + "?");
+			NextGuess();
 		} else if (Input.GetKeyDown(KeyCode.Return)) {
 			print("I won!");
 		}
+	}
+
+	void StartGame()
+	{
+		max++;
+		//guess = (max + min) / 2;
+		print("Welcome to Number Wizard");
+		print("Pick a number in your head, but don't tell me!");
+
+		print("The highest number you can pick is " + max);
+		print("The lowest number you can pick is " + min);
+
+		print("Is the number higher or lower than " + guess + "?");
+		print("Up = higher, Down = lower, return = equal");
+	}
+
+	void NextGuess()
+	{
+		guess = (max + min) / 2;
+		print("Higher or lower than " + guess + "?");
+		print("Up = higher, Down = lower, return = equal");
 	}
 }
