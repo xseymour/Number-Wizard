3a95ad2b88d19aba42cd540c70d8bfdcf6613a97
Xavier Seymour <xseymour3@gmail.com>
2018-05-14 14:30:45 -0400

    23. Completing Number Wizard
    Backend built

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -3,15 +3,34 @@ using System.Collections;
 
 public class NumberWizard : MonoBehaviour {
 
-	public int min = 1;
-	public int max = 1000;
-	public int guess = 500;
+	public int min;
+	public int max;
+	public int guess;
 
 	// Use this for initialization
 	void Start () {
 		StartGame();
 	}
-	
+
+	void StartGame()
+	{
+		min = 1;
+		max = 1000;
+		guess = 500;
+		//guess = (max + min) / 2;
+		print("===========================");
+		print("Welcome to Number Wizard");
+		print("Pick a number in your head, but don't tell me!");
+
+		print("The highest number you can pick is " + max);
+		print("The lowest number you can pick is " + min);
+
+		print("Is the number higher or lower than " + guess + "?");
+		print("Up = higher, Down = lower, return = equal");
+
+		max++;
+	}
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.UpArrow)) {
@@ -22,21 +41,8 @@ public class NumberWizard : MonoBehaviour {
 			NextGuess();
 		} else if (Input.GetKeyDown(KeyCode.Return)) {
 			print("I won!");
+			StartGame();
 		}
-	}
-
-	void StartGame()
-	{
-		max++;
-		//guess = (max + min) / 2;
-		print("Welcome to Number Wizard");
-		print("Pick a number in your head, but don't tell me!");
-
-		print("The highest number you can pick is " + max);
-		print("The lowest number you can pick is " + min);
-
-		print("Is the number higher or lower than " + guess + "?");
-		print("Up = higher, Down = lower, return = equal");
 	}
 
 	void NextGuess()
